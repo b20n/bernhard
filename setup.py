d5b393b626d3a247b8a13f19e729c5ec2877f0ec
Benjamin Anderson <b@banjiewen.net>
2014-07-07 08:56:17 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name = 'bernhard',
-    version = '0.1.0',
+    version = '0.1.1',
 
     description = 'Python client for Riemann',
     long_description = file(
