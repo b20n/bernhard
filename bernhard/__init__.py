c8d615e9d980de02f0360c00ce86391397f04eb7
Bryon Roche <bryon.roche@rd.io>
2015-07-21 11:37:41 -0700

    Have bernhard attempt to set SO_REUSEADDR.
    On an app with heavy TCP traffic, this will minimize the use of extra
    TCP IP/port pairs.

--- bernhard/__init__.py
+++ bernhard/__init__.py
@@ -35,6 +35,10 @@ class TCPTransport(object):
                 self.sock = None
                 continue
             try:
+                self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            except (socket.error, AttributeError) as e:
+                log.exception("Could set set REUSEADDR on tcp client socket: %s", e)
+            try:
                 self.sock.connect(sa)
             except socket.error as e:
                 log.exception("Exception connecting to TCP socket: %s", e)
